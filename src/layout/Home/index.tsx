8a0069921874d96fbf354e978899feb202c4c69e
Caio Sousa <caiodelimack@hotmail.com>
2022-08-19 00:03:44 -0300

    feat: adicionando lib ao projeto

--- src/layout/Home/index.tsx
+++ src/layout/Home/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-import { Container } from '../../components';
-import { Services, HowItWorks, Contacts } from '../../modules';
+import { Container } from "../../components";
+import { Services, HowItWorks, Contacts } from "../../modules";
 
 export const Home = () => (
-  <Container direction="column" width="block" maxWidth="block" gap={[20, 30]} padding='0'>
+  <Container
+    direction="column"
+    width="block"
+    maxWidth="block"
+    gap={[20, 30]}
+    padding="0"
+  >
     <>
       <Services />
       <HowItWorks />
       <Contacts />
     </>
   </Container>
-)
+);
